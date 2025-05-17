d08b69f84bbb857bb56f5184a5cd2d6e653b4924
Anahi Cabrera <anahicabreracalisaya2@gmail.com>
2025-05-17 13:25:39 -0400

    Mi juego favorito CACHO

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,8 @@
+# Juego Cacho Boliviano
+print("1. Cacho se juega con un dado de seis caras y generalmente en grupos de dos o más jugadores")
+print("2. Cada jugador lanza los dados en su turno, tratando de obtener combinaciones específicas para ganar puntos o avanzar")
+print("3. Las combinaciones comunes incluyen pareja, tríos, poker, full, escalera, entre otras, similares al póker, pero adaptadas")
+print("4. En cada turno, el jugador puede tirar hasta tres veces, guardando o relanzando los dados para mejorar su combinación")
+print("5. La puntuación se registra según la combinación lograda en cada turno, siguiendo una tabla predeterminada")
+print("6. El objetivo es alcanzar una puntuación determinada, por ejemplo, 25 o 50 puntos, antes que los otros jugadores")
+print("10. El que acumula más puntos o logra un cacho especial gana la partida")
