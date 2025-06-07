a2b198e0e56ec1960faa877b6b11e214504f1635
Anahi Cabrera <anahicabreracalisaya2@gmail.com>
2025-06-06 22:45:11 -0400

    Restando

---
+++ retos_sesion_07/ejercicio02.py
@@ -0,0 +1,5 @@
+entero1 = int(input("Ingrese el primer valor entero: "))
+entero2 = int(input("Ingrese el segundo valor entero: "))
+entero3 = int(input("Ingrese el tercer valor entero: "))
+resta= entero1-entero2-entero3
+print("el resultado es:", resta)
