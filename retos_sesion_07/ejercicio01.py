220f960463b4c839065dc15b9aa0e501f13a6f2d
Anahi Cabrera <anahicabreracalisaya2@gmail.com>
2025-06-06 22:32:43 -0400

    Mi frase

---
+++ retos_sesion_07/ejercicio01.py
@@ -0,0 +1,5 @@
+frase= "Ser tú misma es un regalo...\n"\
+"Y quienes critican, normalmente están lidiando con sus propias inseguridades. " \
+"No te dejes apagar por opiniones ajenas. " \
+"Brilla a tu manera, ríe fuerte, comparte tus ideas y sé fiel a ti misma."
+print(frase)
