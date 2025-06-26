7c13e822a69d0b24c91b2baa9198643566d99eaa
Anahi Cabrera <anahicabreracalisaya2@gmail.com>
2025-06-25 22:55:13 -0400

    Comparación de pares e impares

--- retos_sesion_06/ejercicio_05.py
+++ retos_sesion_06/ejercicio_05.py
@@ -2,5 +2,14 @@ num1=44
 num2=98
 num3=111
 num4=333
+print("El primer par es par:")
 print(((num1) %2 ==0 ) or ((num2)%2==0))
+print("El segundo par es par:")
 print(((num3) %2 ==0 ) or ((num4)%2==0))
+print("El primer par es impar")
+print(((num1) %2 !=0 ) or ((num2)%2!=0))
+print("El segundo par es impar:")
+print(((num3) %2 !=0 ) or ((num4)%2!=0))
+
+
+
