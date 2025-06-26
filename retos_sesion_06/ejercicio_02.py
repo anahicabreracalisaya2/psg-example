3b68c17d2956ff9c04bc7923b9a67bf61ab891dd
Anahi Cabrera <anahicabreracalisaya2@gmail.com>
2025-06-25 23:05:09 -0400

    Operador XNOR

--- retos_sesion_06/ejercicio_02.py
+++ retos_sesion_06/ejercicio_02.py
@@ -2,3 +2,12 @@ print ("Operador XNOR")
 a = True
 b = True
 print (not((a or b) and not (a and b)))
+a = True
+b = False
+print (not((a or b) and not (a and b)))
+a = False
+b = True
+print (not((a or b) and not (a and b)))
+a = False
+b = False
+print (not((a or b) and not (a and b)))
