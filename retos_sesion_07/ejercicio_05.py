7b7b97acd072cb1c52a80223500e50c997236d73
Anahi Cabrera <anahicabreracalisaya2@gmail.com>
2025-06-26 22:59:21 -0400

    Es un palindromo

--- retos_sesion_07/ejercicio_05.py
+++ retos_sesion_07/ejercicio_05.py
@@ -1,5 +1,4 @@
-cadena = input("Ingresa una cadena: ")
+cadena = input("Ingresa una cadena:")
 cadena_limpia = cadena.replace(" ", "").lower()
 es_palindromo = cadena_limpia == cadena_limpia[::-1]
-
 print(cadena,"es", es_palindromo)
