00dad6bd1d790f4c7d10e3c802286071cb3b459a
Anahi Cabrera <anahicabreracalisaya2@gmail.com>
2025-07-05 12:39:40 -0400

    Mis prendas

---
+++ retos_sesion_10/ejercicio_02.py
@@ -0,0 +1,8 @@
+cad1 = ["Short", "Playera", "Sudadera", "Tenis", "Short", "Calcetines"]
+cad2= ["Saco", "Corbata", "Pantalón de vestir", "Zapatos", "Calcetines"]
+con1= set(cad1)
+con2=set(cad2)
+prendas=con1.union(cad2)
+print(con1)
+print(con2)
+print(prendas)
