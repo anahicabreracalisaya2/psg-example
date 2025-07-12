d15132f488ff7dd42ea4a6fb4dcf650a7d52316d
Anahi Cabrera <anahicabreracalisaya2@gmail.com>
2025-07-12 11:43:27 -0400

    Especie marina

---
+++ retos_sesion_11/ejercicio_01.py
@@ -0,0 +1,8 @@
+# Diccionario con la información de la estrella de mar
+estrella_mar = {
+    "especie": "Estrella de mar","habitat": "zona rocosas y agua marina",
+    "dieta": "moluscos y pequeños crustáceos","estado_de_salud": "regular","edad": 7
+}
+personal = {"Sara", "Pedro", "Diego"}
+estrella_mar['responsables']=personal
+print(estrella_mar)
