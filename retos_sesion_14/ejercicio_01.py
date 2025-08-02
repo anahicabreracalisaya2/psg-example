535fe625860e5dc28179d770de5768708cc8f6b2
Anahi Cabrera <anahicabreracalisaya2@gmail.com>
2025-08-02 12:59:53 -0400

    Promedio de notas

---
+++ retos_sesion_14/ejercicio_01.py
@@ -0,0 +1,10 @@
+def promedio(calificaciones):
+    total = sum(calificaciones)
+    cantidad = len(calificaciones)
+    promedio = total / cantidad
+    return promedio
+
+# datos y llamada de la función
+calificaciones = [50, 75, 80, 91, 70]
+promedio = promedio(calificaciones)
+print("El promedio de las calificaciones es:", promedio)
