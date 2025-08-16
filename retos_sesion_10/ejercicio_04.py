44861122ef603aed573f4e049a4afc951635e5f7
Anahi Cabrera <anahicabreracalisaya2@gmail.com>
2025-08-16 00:04:33 -0400

    Compatibilidad

--- retos_sesion_10/ejercicio_04.py
+++ retos_sesion_10/ejercicio_04.py
@@ -1,7 +1,10 @@
 jane={'Lemon Pie', 'Brownie', 'Tarta de Manzana',' Helado de Chocolate', 'Flan'}
-Jhon={'Carrot Cake', 'Croissant de Chocolate','Lemon Pie',"Brownie de Manzana", "Pudding"} 
-largo=len(jane)
-aux=jane.intersection(Jhon)
-laux=len(aux)
-aux2=laux<=(largo/2)
-print("¿Deben replantear su relación?", aux2)
+jhon={'Carrot Cake', 'Croissant de Chocolate','Lemon Pie',"Brownie de Manzana", "Pudding"} 
+
+comunes = jane & jhon
+total_postres = jane | jhon
+compatibilidad = len(comunes) / len(total_postres) * 100
+replantear = compatibilidad <= 50
+
+print("¿Deben replantear su relación?",replantear)
+print("Debido a que su ompatibilidad es del:",compatibilidad,"%")
