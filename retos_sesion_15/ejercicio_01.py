5e9bb106e20c9bcada857352e9ac5c5a5abb92a1
Anahi Cabrera <anahicabreracalisaya2@gmail.com>
2025-08-16 22:35:09 -0400

    Calculadora potencial

--- retos_sesion_15/ejercicio_01.py
+++ retos_sesion_15/ejercicio_01.py
@@ -3,25 +3,28 @@ def division(num1, num2):
 
 while True:
     try:
-        a = float(input("Ingrese el primer número: "))
+        a = input("Ingrese el primer número: ")
         if a == "salir":
             break
-        b = float(input("Ingrese el segundo número: "))
+        b = input("Ingrese el segundo número: ")
         if b == "salir":
             break
-        suma=a+b
-        resta=a-b
-        multi=a*b
-        div = division(a, b)
-    except ZeroDivisionError as e:
-        print("0️⃣ Error:", e, type(e))
+        a = float(a)
+        b = float(b)
+
+        suma = a + b
+        resta = a - b
+        multi = a * b
+        print("😺 Resultado de la suma: ", suma)
+        print("😺 Resultado de la resta: ", resta)
+        print("😺 Resultado de la multiplicación: ", multi)
+        try:
+            div = division(a, b)
+            print("😺 Resultado de la división: ", div)
+        except ZeroDivisionError as e:
+            print("0️⃣ Error:", e, type(e))
+
     except TypeError as e:
         print(" 🤔 Error:", e, type(e))
     except Exception as e:
         print(" 😒 Error:", e, type(e))
-    else:
-        print("😺 Resultado de la suma: ",suma)
-        print("😺 Resultado de la resta: ",resta)
-        print("😺 Resultado de la multiplicacion: ",multi)
-        print("😺 Resultado de la division: ",div)
-        
